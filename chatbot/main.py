b425c72bf1ad6c7b3a7d62187d5bf62dbc74625f
slavvok <slavvok@gmail.com>
2023-01-29 05:29:31 +0400

    Add chat monitoring function

--- chatbot/main.py
+++ chatbot/main.py
@@ -4,9 +4,10 @@ import re
 from vkbottle.polling import BotPolling
 from vkbottle.api import API
 from vkbottle.bot import Bot
-from vkbottle_types.events import GroupEventType
+from vkbottle_types.events import GroupEventType, UserEventType
+from vkbottle_types.objects import MessagesForward
 
-from model import Event
+from model import Event, Message
 
 TOKEN = os.environ.get("API_TOKEN")
 HASHTAGS = os.environ.get("HASHTAGS")
@@ -24,6 +25,20 @@ async def wall_updates(event: Event):
     post = f"wall{event.object.owner_id}_{event.object.id}"
     if hashtags_pattern.findall(event.object.text.lower()):
         await api.messages.send(attachment=post, user_id=RECIPIENTS, random_id=0)
+
+
+@bot.on.raw_event(event=[GroupEventType.MESSAGE_NEW, UserEventType.MESSAGE_NEW], dataclass=Message)
+async def chat_updates(event: Message):
+    if hashtags_pattern.findall(event.object.message.text.lower()):
+        message = event.object.message
+        obj = {
+            "peer_id": message.peer_id,
+            "conversation_message_ids": [message.conversation_message_id,],
+        }
+        data = MessagesForward(
+            **obj
+        )
+        await api.messages.send(forward=data.json(), user_id=RECIPIENTS, random_id=0)
 
 
 if __name__ == '__main__':
