6c4607e8c519a009a3df897d7ff98d5f69c4a0ba
slavvok <slavvok@gmail.com>
2023-01-29 05:29:31 +0400

    Add chat monitoring function

--- chatbot/model.py
+++ chatbot/model.py
@@ -20,3 +20,25 @@ class Event(BaseGroupEvent, ABC):
         allow_mutation = True
         validate_assignment = True
         arbitrary_types_allowed = True
+
+
+class MessageObjectModel(BaseModel):
+    conversation_message_id: Optional[int]
+    peer_id: Optional[int]
+    date: Optional[int]
+    from_id: Optional[int]
+    event_id: Optional[str]
+    text: Optional[str]
+
+
+class MessageObject(BaseModel):
+    message: Optional[MessageObjectModel]
+
+
+class Message(BaseGroupEvent, ABC):
+    object: Optional[MessageObject]
+
+    class Config:
+        allow_mutation = True
+        validate_assignment = True
+        arbitrary_types_allowed = True
